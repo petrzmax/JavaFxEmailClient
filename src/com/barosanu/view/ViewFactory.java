93efa3771eda74f9f2b2fd794058a0c0e4353b3d
Artur Petrzak <petrzmax@gmail.com>
2021-04-12 14:24:48 +0200

    Stages initialization

--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.barosanu.view;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.BaseController;
+import com.barosanu.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -12,5 +20,22 @@ public class ViewFactory {
 
     public void showLoginWindow() {
         System.out.println("Show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
