c8d845b3fab0aeab9c4e8419bd732bbe9aa4a377
Artur Petrzak <petrzmax@gmail.com>
2021-04-14 13:15:24 +0200

    Create & pass folder list to folder updater service

--- src/com/barosanu/EmailManager.java
+++ src/com/barosanu/EmailManager.java
@@ -1,11 +1,18 @@
 package com.barosanu;
 
 import com.barosanu.controller.services.FetchFolderService;
+import com.barosanu.controller.services.FolderUpdaterService;
 import com.barosanu.model.EmailAccount;
 import com.barosanu.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+
+    private FolderUpdaterService folderUpdaterService;
 
     // Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
@@ -14,9 +21,21 @@ public class EmailManager {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
+
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
+
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem);
+        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem, folderList);
         fetchFolderService.start();
 
         foldersRoot.getChildren().add(treeItem);
--- src/com/barosanu/controller/services/FetchFolderService.java
+++ src/com/barosanu/controller/services/FetchFolderService.java
@@ -9,15 +9,18 @@ import javax.mail.MessagingException;
 import javax.mail.Store;
 import javax.mail.event.MessageCountEvent;
 import javax.mail.event.MessageCountListener;
+import java.util.List;
 
 public class FetchFolderService extends Service<Void> {
 
     private Store store;
     private EmailTreeItem<String> foldersRoot;
+    private List<Folder> folderList;
 
-    public FetchFolderService(Store store, EmailTreeItem<String> foldersRoot) {
+    public FetchFolderService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store = store;
         this.foldersRoot = foldersRoot;
+        this.folderList = folderList;
     }
 
     @Override
@@ -40,6 +43,8 @@ public class FetchFolderService extends Service<Void> {
     private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
 
         for(Folder folder: folders) {
+            folderList.add(folder);
+
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
