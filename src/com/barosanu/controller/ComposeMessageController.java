102f547f90922ccb0f5896614d29e36ca4c021ab
Artur Petrzak <petrzmax@gmail.com>
2021-04-16 13:30:52 +0200

    Fix code

--- src/com/barosanu/controller/ComposeMessageController.java
+++ src/com/barosanu/controller/ComposeMessageController.java
@@ -1,38 +1,49 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
 
-public class ComposeMessageController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ComposeMessageController extends BaseController implements Initializable {
 
 
-    public class ComposeMessageWindow {
+    @FXML
+    private TextField subjectTextField;
 
-        @FXML
-        private TextField subjectTextField;
+    @FXML
+    private TextField recipientTextField;
 
-        @FXML
-        private TextField recipientTextField;
+    @FXML
+    private HTMLEditor htmlEditor;
 
-        @FXML
-        private HTMLEditor htmlEditor;
+    @FXML
+    private Label errorLabel;
 
-        @FXML
-        private Label errorLabel;
-
-        @FXML
-        void sendButtonAction() {
-            System.out.println("TEST WYSYLANKA");
-        }
-
-    }
-
+    @FXML
+    private ChoiceBox<EmailAccount> emailAccountChoice;
 
     public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
     }
+
+    @FXML
+    void sendButtonAction() {
+        System.out.println("TEST WYSYLANKA");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        emailAccountChoice.setItems(emailManager.getEmailAccounts());
+        emailAccountChoice.setValue(emailManager.getEmailAccounts().get(0));
+    }
+
 }
