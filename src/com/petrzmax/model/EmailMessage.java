93335cc287ab7d94eff8a2e8eaf751033a74409e
Artur Petrzak <petrzmax@gmail.com>
2021-04-20 17:09:44 +0200

    change package name

---
+++ src/com/petrzmax/model/EmailMessage.java
@@ -0,0 +1,85 @@
+package com.petrzmax.model;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import javax.mail.Message;
+import javax.mail.internet.MimeBodyPart;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class EmailMessage {
+
+    private SimpleStringProperty subject;
+    private SimpleStringProperty sender;
+    private SimpleStringProperty recipient;
+    private SimpleObjectProperty<SizeInteger> size;
+    private SimpleObjectProperty<Date> date;
+    private boolean isRead;
+    private Message message;
+    private List<MimeBodyPart> attachmentList = new ArrayList<MimeBodyPart>();
+    private boolean hasAttachments = false;
+
+    public EmailMessage(String subject, String sender, String recipient, int size, Date date, boolean isRead, Message message) {
+        this.subject = new SimpleStringProperty(subject);
+        this.sender = new SimpleStringProperty(sender);
+        this.recipient = new SimpleStringProperty(recipient);
+        this.size = new SimpleObjectProperty<SizeInteger>(new SizeInteger(size));
+        this.date = new SimpleObjectProperty<Date>(date);
+        this.isRead = isRead;
+        this.message = message;
+    }
+
+    public String getSubject() {
+        return subject.get();
+    }
+
+    public String getSender() {
+        return sender.get();
+    }
+
+    public String getRecipient() {
+        return recipient.getName();
+    }
+
+    public SizeInteger getSize() {
+        return size.get();
+    }
+
+    public Date getDate() {
+        return date.get();
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void addAttachment(MimeBodyPart mbp) {
+        hasAttachments = true;
+        if(!attachmentList.contains(mbp)){
+            attachmentList.add(mbp);
+            try {
+                System.out.println("Added attach: " + mbp.getFileName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean hasAttachments() {
+        return hasAttachments;
+    }
+
+    public List<MimeBodyPart> getAttachmentList() {
+        return attachmentList;
+    }
+}
---
+++ src/com/petrzmax/view/ViewFactory.java
@@ -0,0 +1,116 @@
+package com.petrzmax.view;
+
+import com.petrzmax.EmailManager;
+import com.petrzmax.controller.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ViewFactory {
+
+    //View options handling:
+    private ColorTheme colorTheme = ColorTheme.DEFAULT;
+    private FontSize fontSize = FontSize.MEDIUM;
+
+    private EmailManager emailManager;
+    private ArrayList<Stage> activeStages;
+
+    private boolean mainViewInitialized = false;
+
+    public ViewFactory(EmailManager emailManager) {
+        this.emailManager = emailManager;
+        activeStages = new ArrayList<>();
+    }
+
+    public ColorTheme getColorTheme() {
+        return colorTheme;
+    }
+
+    public void setColorTheme(ColorTheme colorTheme) {
+        this.colorTheme = colorTheme;
+    }
+
+    public FontSize getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(FontSize fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public void showLoginWindow() {
+        System.out.println("Show login window called");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        initializeStage(controller);
+    }
+
+    public void showOptionsWindow() {
+        System.out.println("Show options window called");
+
+        BaseController controller = new OptionsWindowController(emailManager, this, "OptionsWindow.fxml");
+        initializeStage(controller);
+    }
+
+    public void showMainWindow() {
+        System.out.println("Show main window called");
+
+        BaseController controller = new MainWindowController(emailManager, this, "MainWindow.fxml");
+        initializeStage(controller);
+        mainViewInitialized = true;
+    }
+
+    public void showComposeMessageWindow() {
+        System.out.println("Show composer message window called");
+
+        BaseController controller = new ComposeMessageController(emailManager, this, "ComposeMessageWindow.fxml");
+        initializeStage(controller);
+    }
+
+    public void showEmailDetailsWindow() {
+        BaseController controller = new EmailDetailsWindowController(emailManager, this, "EmailDetailsWindow.fxml");
+        initializeStage(controller);
+    }
+
+    private void initializeStage(BaseController baseController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
+        fxmlLoader.setController(baseController);
+        Parent parent;
+
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+        activeStages.add(stage);
+    }
+
+    public void closeStage(Stage stageToClose) {
+        stageToClose.close();
+        activeStages.remove(stageToClose);
+    }
+
+    public void updateStyles() {
+        for (Stage stage: activeStages) {
+            Scene scene = stage.getScene();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(getClass().getResource(ColorTheme.getCssPath(colorTheme)).toExternalForm());
+            scene.getStylesheets().add(getClass().getResource(FontSize.getCssPath(fontSize)).toExternalForm());
+        }
+    }
+
+    public boolean isMainViewInitialized() {
+        return mainViewInitialized;
+    }
+}
